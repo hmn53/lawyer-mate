205635c04b5dc41ece9a9bda8d59d81dd79f56b6
hmn53 <hatim.nalawala987@gmail.com>
2020-02-25 16:22:04 +0530

    added appointment

--- back-end/laravel/app/Http/Controllers/PagesController.php
+++ back-end/laravel/app/Http/Controllers/PagesController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\SendMailable;
 use DB;
 use App\Events\ReminderAdded;
 use App\CaseTable;
@@ -11,144 +13,204 @@ use App\Doc;
 use App\Reminder;
 use App\Client;
 use Carbon\Carbon;
+use Auth;
+use App\Appointment;
+use App\Directory;
 
 class PagesController extends Controller
 {
     public function lawbook(){
-        return view('pages/lawbook');
+        if(Auth::check())
+            return view('pages/lawbook');
+        else
+            return redirect("/login");
     }
 
     public function index(){
+        if(Auth::check())
+            return redirect("/dashboard");
         return view('pages/index');
     }
 
     public function docs(){
-        return view('pages.docs');
+        if(Auth::check())
+            return view('pages.docs');
+        else
+            return redirect("/login");
     }
 
     public function uploaddocs(){
-        return view('pages/upload');
+        if(Auth::check())
+            return view('pages/upload');
+        else
+            return redirect("/login");
     }
 
     public function addcase(){
-        return view('pages/addcase');
+        if(Auth::check())
+            return view('pages/addcase');
+        else
+            return redirect("/login");
     }
 
     public function clients(){
-        return view('pages/clients');
+        if(Auth::check())
+            return view('pages/clients');
+        else
+            return redirect("/login");
     }
 
     public function reminder(){
-        $user = auth()->user();
-        $data = Reminder::where('user_id',$user->id)->get();
-        if(strcmp($user->type,"lawyer")==0){
-            $client_id=Client::select('client_id')->where('lawyer_id',$user->id)->get();
-            $a = array_values($client_id->all());
-            $data_client = Reminder::whereIn('user_id',$a)->get();
-            return view('pages/reminder')->with('data',$data)->with('data_client',$data_client);
-        }
-        return view('pages.reminder')->with('data',$data);
+        if(Auth::check()){
+            $user = auth()->user();
+            $data = Reminder::where('user_id',$user->id)->get();
+            if(strcmp($user->type,"lawyer")==0){
+                $client_id=Client::select('client_id')->where('lawyer_id',$user->id)->get();
+                $a = array_values($client_id->all());
+                $data_client = Reminder::whereIn('user_id',$a)->get();
+                return view('pages/reminder')->with('data',$data)->with('data_client',$data_client);
+            }
+            return view('pages.reminder')->with('data',$data);
+        }else
+            return redirect("/login");
+
     }
 
+
     public function cases(){
-        return view('pages/cases');
+        if(Auth::check())
+            return view('pages/cases');
+        else
+            return redirect("/login");
     }
 
     public function home(){
-        $user = auth()->user();
-        if(strcmp($user->type,"admin")==0){
-            return view('pages/admin');
-        }
-        else
-            return view('pages/dashboard');
+        if(Auth::check()){
+            $user = auth()->user();
+            if(strcmp($user->type,"admin")==0){
+                return view('pages/admin');
+            }
+            else
+                return view('dashboard');
+        } else
+            return redirect("/login");
+
     }
 
-    public function currentcases(){
-        return view('pages/currentcases');
-    }
+    // public function currentcases(){
+    //     return view('pages/currentcases');
+    // }
 
     public function store(Request $request){
-        $request->validate([
-            'caseno' => 'required',
-            'description' => 'required',
-        ]);
-        $name = $request->input('clientname');
-        $client_id = DB::table('customusers')->select('id')->where('name',$name)->get();
-        
-        $result= json_decode($client_id,true);
-        
-         foreach($result as $data){
-             $id = $data['id'];
-         }
-         
-        $var = new CaseTable;
-        $var->case_no=$request->input('caseno');
-        $var->lawyer_id = auth()->user()->id;
-        $var->client_id= $id;
-        $var->judge_name= $request->input('judgename');
-        $var->court_type = $request->input('courttype');
-        $var->category = $request->input('category');
-        $var->case_members = $request->input('casemembers');
-        $var->status = $request->input('status');
-        $var->date_of_filing = $request->input('filingdate');
-        $var->opponent = $request->input('opponent');
-        $var->description = $request->input('description');
-        $var->summary = $request->input('summary');
-        $var->background = $request->input('background');
-        $var->save();
+        if(Auth::check()){
+            $request->validate([
+                'caseno' => 'required',
+                'description' => 'required',
+            ]);
+            $name = $request->input('clientname');
+            $client_id = DB::table('customusers')->select('id')->where('name',$name)->get();
 
-        return redirect('/dashboard');
+            $result= json_decode($client_id,true);
+
+             foreach($result as $data){
+                 $id = $data['id'];
+             }
+
+            $var = new CaseTable;
+            $var->case_no=$request->input('caseno');
+            $var->lawyer_id = auth()->user()->id;
+            $var->client_id= $id;
+            $var->judge_name= $request->input('judgename');
+            $var->court_type = $request->input('courttype');
+            $var->category = $request->input('category');
+            $var->case_members = $request->input('casemembers');
+            $var->status = $request->input('status');
+            $var->date_of_filing = $request->input('filingdate');
+            $var->opponent = $request->input('opponent');
+            $var->description = $request->input('description');
+            $var->summary = $request->input('summary');
+            $var->background = $request->input('background');
+            $var->save();
+
+            return redirect('/dashboard');
+        }
+
     }
     public function storeReminder(Request $request){
-        $request->validate([
-            // 'case_no' => 'required',
-            'description' => 'required',
-            'type' => 'required',
-            'reminddate'=>'required',
-        ]);
-        $var = new Reminder;
-       $var->user_id =auth()->user()->id;
-       $var->case_no = $request->input('case_no');
-       $var->description = $request->input('description');
-       $var->remind_date = $request->input('reminddate');
-       $var->type = $request->input('type');
-        $var->save();
-        event(new ReminderAdded(auth()->user()->id,auth()->user()->name,'Appointment',auth()->user()->type,$request->input('description'),Carbon::now()->toDateTimeString()));
-        return redirect('/dashboard');
+        if(Auth::check()){
+            $request->validate([
+                // 'case_no' => 'required',
+                'description' => 'required',
+                'type' => 'required',
+                'reminddate'=>'required',
+            ]);
+            $var = new Reminder;
+           $var->user_id =auth()->user()->id;
+           $var->case_no = $request->input('case_no');
+           $var->description = $request->input('description');
+           $var->remind_date = $request->input('reminddate');
+           $var->type = $request->input('type');
+            $var->save();
+            event(new ReminderAdded(auth()->user()->id,auth()->user()->name,'Appointment',auth()->user()->type,$request->input('description'),Carbon::now()->toDateTimeString()));
+            return redirect('/dashboard');
+        }
+
+    }
+
+    public function appointment(Request $request){
+        if(Auth::check()){
+            $request->validate([
+                'title' => 'required',
+                'description' => 'required',
+                'date'=>'required',
+            ]);
+            $var = new Appointment;
+           $var->user_id =auth()->user()->id;
+           $var->title = $request->input('title');
+           $var->lawyer_name = $request->input('lawyer_name');
+           $var->description = $request->input('description');
+           $var->date = $request->input('date');
+           $var->save();
+           return redirect('/dashboard');
+        }
     }
     public function upload(Request $request){
-        $request->validate([
-            'caseno' => 'required',
-            'description' => 'required',
-            'docname' => 'required',
-            'doc' => 'file|max:1999',
-        ]);
-        $user_id = auth()->user()->id;
-        $var = new Doc;
-        $var->upload_by = $user_id;
-        $var->case_id = $request->input('caseno');
-        $var->document_name = $request->input("docname");
-        $var->description = $request->input("description");
-        //$var->upload_on = $date;
-        $var->path = $request->doc->getClientOriginalName();
-        $var->save();
-        $path= $request->doc->storeAs('docs', $request->doc->getClientOriginalName());
-        return redirect('/docs');
-
+        if(Auth::check()) {
+            $request->validate([
+                'caseno' => 'required',
+                'description' => 'required',
+                'docname' => 'required',
+                'doc' => 'file|max:1999',
+            ]);
+            $user_id = auth()->user()->id;
+            $var = new Doc;
+            $var->upload_by = $user_id;
+            $var->case_id = $request->input('caseno');
+            $var->document_name = $request->input("docname");
+            $var->description = $request->input("description");
+            //$var->upload_on = $date;
+            $var->path = $request->doc->getClientOriginalName();
+            $var->save();
+            $path= $request->doc->storeAs('docs', $request->doc->getClientOriginalName());
+            return redirect('/docs');
+        }
     }
-   
 
-    public function services(){
-        $var = array(
-            'title'=>'Services',
-            'services'=>['Web','Android','Node','C++','Javascript','Java','C']
-        );
-        return view('pages/services')->with($var);
-    }
+
+    // public function services(){
+    //     $var = array(
+    //         'title'=>'Services',
+    //         'services'=>['Web','Android','Node','C++','Javascript','Java','C']
+    //     );
+    //     return view('pages/services')->with($var);
+    // }
 
     public function profile()
     {
-        return view('pages/profile');
+        if(Auth::check())
+            return view('pages/profile');
+        else
+            return redirect("/login");
     }
 
     //admin
@@ -244,4 +306,28 @@ class PagesController extends Controller
         $doc->delete();
         return redirect('/admin/reminder');
     }
+
+    //mail
+    public function emailSend()
+    {
+        $name = 'Hatim';
+        Mail::to('hatim.nalawala987@gmail.com')->send(new SendMailable($name));
+        return 'Email was sent';
+    }
+
+    //search lawbook
+    public function searchLawbook(Request $request){
+        $q = $request->input('searchTerm');
+        $laws = Directory::where('penal_code','LIKE','%'.$q.'%')->get();
+        if(count($laws) > 0)
+            return view('pages.lawbook')->with('laws',$laws)->withQuery ( $q );
+        else{
+            $laws = Directory::where('name','LIKE','%'.$q.'%')->orWhere('detail','LIKE','%'.$q.'%')->get();
+            if(count($laws) > 0)
+                return view('pages.lawbook')->with('laws',$laws)->withQuery ( $q );
+            else 
+                return view ('pages.lawbook')->withMessage('No Details found. Try to search again !');
+        }
+
+}
 }
--- back-end/laravel/routes/web.php
+++ back-end/laravel/routes/web.php
@@ -5,6 +5,10 @@ use App\Doc;
 use App\CaseTable;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\ReminderDue;
+use App\CustomUser;
+use App\Appointment;
+use App\Directory;
+use App\Notifications\ReminderNotifications;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -22,6 +26,9 @@ Route::get('/', function () {
 });
 */
 $user = auth()->user();
+
+Auth::routes(['verify' => true]);
+
 Route::get('/', 'PagesController@index');
 
 Route::get('/lawbook','PagesController@lawbook');
@@ -46,6 +53,11 @@ Route::get('clients', 'PagesController@clients');
 Route::get('/lawyers', function () {
     return view('pages/lawyers');
 });
+Route::get('/appointment', function () {
+    return view('pages/appointment');
+});
+
+Route::post('/appointment','PagesController@appointment');
 Route::post('/cases/add','PagesController@store');
 Route::post('/docs/upload','PagesController@upload');
 Route::get('/cases', 'PagesController@cases');
@@ -56,6 +68,10 @@ Route::get('/cases/show/{id}',function($id){
 });
 Route::get('download/{path}', function ($path) {
     return response()->download(storage_path('app/docs/' . $path));
+});
+
+Route::get('view/{path}',function($path){
+    return response()->file(storage_path('app/docs/' . $path));
 });
 
 Route::get('/admin/cases', function () {
@@ -84,11 +100,27 @@ Route::get('/email', function () {
 	});
 });
 
+Route::get('/search/lawyers', function () {
+    return view('pages.searchLawyers');
+});
+
 //docs
 Route::get('/docs/verify/{id}', function ($id) {
     $updateDoc = Doc::find($id);
     $updateDoc->verified = "verified";
+    $client = CustomUser::find($updateDoc->upload_by);
+    $document = $updateDoc->document_name;
+    $description = $updateDoc->description;
+    $caseID = $updateDoc->case_id;
     $updateDoc->save();
+    $clientName = $client->name;
+    $clientEmail = $client->email;
+    $data = array('name'=>$clientName, 'email'=>$clientEmail ,'document' =>$document,'caseID'=>$caseID, 'description'=>$description);
+    Mail::send('email.doc', $data, function($message) use ($data) {
+        $message->to($data['email'], $data['name'])->subject
+            ('Document Verified!');
+        $message->from('webportalforlawyers@gmail.com','Hatim');
+    });
     return redirect('/docs');
 });
 
@@ -98,6 +130,35 @@ Route::get('/docs/unverify/{id}', function ($id) {
     $updateDoc->save();
     return redirect('/docs');
 });
+
+//appointment
+Route::get('/appointment/accept/{id}', function ($id) {
+    $updateApp = Appointment::find($id);
+    $updateApp->accepted = "accepted";
+    $user = CustomUser::find($updateApp->user_id);
+    $date = $updateApp->date;
+    $updateApp->save();
+    $name = $user->name;
+    $email = $user->email;
+    $lawyer = auth()->user();
+    $lawyerName = $lawyer->name;
+    $lawyerEmail = $lawyer->email;
+    $data = array('name'=>[$lawyerName,$name], 'email'=>[$lawyerEmail,$email],'date'=>$date);
+    Mail::send('email.appointment', $data, function($message) use ($data) {
+        $message->to($data['email'], $data['name'])->subject
+            ('Appointment Scheduled!');
+        $message->from('webportalforlawyers@gmail.com','Hatim');
+    });
+    return redirect('/dashboard');
+});
+
+Route::get('/appointment/unaccept/{id}', function ($id) {
+    $updateApp = Appointment::find($id);
+    $updateApp->accepted = "rejected";
+    $updateApp->save();
+    return redirect('/dashboard');
+});
+
 //admin
 Route::post('/admin/add', 'PagesController@addAdmin');
 Route::post('/admin/add/case', 'PagesController@addAdminCase');
@@ -110,3 +171,35 @@ Route::post('/admin/remove/client/{id}', 'PagesController@deleteClient');
 Route::post('/admin/remove/doc/{id}', 'PagesController@deleteDoc');
 Route::post('/admin/remove/reminder/{id}', 'PagesController@deleteReminder');
 
+
+//mail
+Route::get('/send/email', 'PagesController@emailSend');
+Route::get('/sendbasicemail','MailController@basic_email');
+Route::get('/sendhtmlemail','MailController@html_email');
+Route::get('welcome/{id}', 'MailController@lawyerWelcome');
+
+//lawbook search
+// Route::post('/search/lawbook', function (Request $request) {
+//     $q = Input::get ( 'searchTerm' );
+//     $laws = Directory::where('name','LIKE','%'.$q.'%')->orWhere('detail','LIKE','%'.$q.'%')->get();
+//     if(count($laws) > 0)
+//         return view('lawbook')->with('laws',$laws)->withQuery ( $q );
+//     else return view ('lawbook')->withMessage('No Details found. Try to search again !');
+// });
+Route::post('/search/lawbook','PagesController@searchLawbook');
+
+//test
+Route::get('/test', function () {
+    $user = auth()->user();
+    //$user->notify(new Reminder("Welcome to the Web Portal for Lawyers."));
+    $name = $user->name;
+        $email = $user->email;
+        $data = array('name'=>$name, 'email'=>$email);
+        Mail::send('email.lawyerWelcome', $data, function($message) use ($data) {
+            $message->to($data['email'], $data['name'])->subject
+                ('Welcome to the Web Portal');
+            $message->from('webportalforlawyers@gmail.com','Hatim');
+        });
+    return view('/dashboard');
+    // return date('Y-d-m');
+});
