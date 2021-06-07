d3bb708e13fbd08c3b759a62a749cc54f5612a6b
hmn53 <hatim.nalawala987@gmail.com>
2021-06-07 06:47:45 +0530

    add migrations

---
+++ back-end/laravel/database/migrations/2020_11_08_065414_add_area_to_lawyer_profile_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddAreaToLawyerProfileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('lawyer_profile', function (Blueprint $table) {
+            $table->string('area');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('lawyer_profile', function (Blueprint $table) {
+            //
+        });
+    }
+}
